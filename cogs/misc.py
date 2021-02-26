000d4d2796782c543943545e04900dc49e3df7d8
aschenkuttel <aschenkuttel@gmail.com>
2021-02-26 11:51:29 +0100

    basic poll command

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import asyncio
 import discord
 import random
 import utils
@@ -7,6 +8,7 @@ import utils
 class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.poll_time = 10
 
     @commands.command(name="summon")
     async def summon_(self, ctx):
@@ -70,8 +72,77 @@ class Utils(commands.Cog):
 
     @commands.command(name="choose")
     async def choose(self, ctx, *arguments):
+        """chooses one random item out of the arguments"""
         item = random.choice(arguments or ["nothing"])
         await ctx.send(f"It's `{item}`")
+
+    @commands.command(name="poll")
+    async def poll_(self, ctx, time: int, *, arguments):
+        """basic poll command, first argument has to be the time
+        in minutes. The rest of the arguments should be separated by
+        newlines, up to 9 choices possible"""
+        lines = arguments.split("\n")
+
+        if not 3 <= len(lines) <= 9:
+            msg = "You should have between two and nine choices"
+            await ctx.send(msg)
+            return
+
+        question = lines.pop(0)
+
+        parsed_options = ""
+        for index, option in enumerate(lines):
+            parsed_options += f"\n`{index + 1}.` {option}"
+
+        title = f"**Poll from {ctx.author.display_name}**"
+        description = f"{title}\n{question}{parsed_options}"
+        embed = discord.Embed(description=description, color=discord.Color.purple())
+        embed.set_footer(text=f"Voting ends in {time} minutes")
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        poll = await ctx.send(embed=embed)
+
+        for num in range(len(lines)):
+            emoji = f"{num + 1}\N{COMBINING ENCLOSING KEYCAP}"
+            await poll.add_reaction(emoji)
+
+        await utils.silencer(ctx.message.delete())
+        whole, remainder = divmod(time, self.poll_time)
+
+        first_duration = self.poll_time if whole else remainder
+        await asyncio.sleep(first_duration * 60)
+
+        for n in range(1, whole):
+            if n + 1 != whole:
+                minutes = self.poll_time
+                cur = (whole - n) * self.poll_time + remainder
+            else:
+                minutes = remainder
+                cur = minutes
+
+            embed.set_footer(text=f"Voting ends in {cur} minutes")
+            await poll.edit(embed=embed)
+            await asyncio.sleep(minutes * 60)
+
+        re_fetched = await ctx.channel.fetch_message(poll.id)
+        votes = sorted(re_fetched.reactions, key=lambda r: r.count, reverse=True)
+        color = discord.Color.red()
+
+        if [r.count for r in votes].count(1) == len(votes):
+            msg = "`Nobody voted!`"
+
+        elif votes[0].count > votes[1].count:
+            color = discord.Color.green()
+            winner = re_fetched.reactions.index(votes[0])
+            msg = f"`{lines[winner]} won with {votes[0].count} votes!`"
+
+        else:
+            msg = "`The poll resulted in a draw...`"
+
+        result = f"{title}\n{question}\n{msg}"
+        result_embed = discord.Embed(description=result, color=color)
+        result_embed.set_footer(text="Finished.")
+        await poll.edit(embed=result_embed)
+        await utils.silencer(poll.clear_reactions())
 
 
 def setup(bot):
